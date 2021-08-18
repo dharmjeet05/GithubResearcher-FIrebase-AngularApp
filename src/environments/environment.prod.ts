a9c9fe9717f188ed88d70438ee225597cf137e8d
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-08-18 09:21:51 +0530

    Bug fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBFw4C4urqiAFBnExe9aoAhUgZ1VMFll6A',
+    authDomain: 'githubapi-angularapp.firebaseapp.com',
+    projectId: 'githubapi-angularapp',
+    storageBucket: 'githubapi-angularapp.appspot.com',
+    messagingSenderId: '955893155266',
+    appId: '1:955893155266:web:f08144d755033fd5c1825b',
+  },
 };
